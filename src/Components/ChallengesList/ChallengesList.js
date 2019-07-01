35693d99425a64c8cb82b42ca6359dabf5b6022e
ryuta <ryuta.eguchi@gmail.com>
2019-07-01 14:04:30 -0700

    Try to hide Create Challenge link for non-group-owner - needs work

--- src/Components/ChallengesList/ChallengesList.js
+++ src/Components/ChallengesList/ChallengesList.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import config from '../../config'
 import TokenService from '../../services/TokenService'
+import GroupListContext from '../../contexts/GroupListContext'
 
 export default class ChallengesList extends React.Component {
+  static contextType = GroupListContext
+
   static defaultProps = {
     challenges: [],
   }
@@ -57,7 +60,7 @@ export default class ChallengesList extends React.Component {
     .then(res => res.json())
     .then(resJson =>
       this.setState({
-        user: resJson.data
+        user: resJson
       })
     )
     .catch(res => {
@@ -71,20 +74,35 @@ export default class ChallengesList extends React.Component {
     )
   }
 
-  render() {
-    // TODO: complete challenge - checkbox interaction with state and API
+  renderCreateChallenge() {
     const { group_id } = this.props
+    return (
+      <Link to={{
+        pathname: '/create-challenge',
+        state: {
+          group_id: group_id
+        }
+      }}>
+        Create Challenge
+      </Link>
+    )
+  }
+
+  // checkUserIsGroupOwner(group_id) {
+  //   this.state.user.user_id == this.context.groupList[group_id - 1].created_by
+  // }
+
+  render() {
+    
+    // TODO: complete challenge - checkbox interaction with state and API
+    const group_id = parseInt(this.props.group_id)
+    const { user_id } = this.state.user
+    const { created_by } = this.context.groupList[(group_id - 1)]
     return (
       <div>
         <h1>Challenge List</h1>
-        <Link to={{
-          pathname: '/create-challenge',
-          state: {
-            group_id: group_id
-          }
-        }}>
-          Create Challenge
-        </Link>
+        {user_id && created_by && (this.state.user.user_id == this.context.groupList[group_id - 1].created_by) && this.renderCreateChallenge()}
+
         {(this.state.user) ? this.renderUserPoints() : this.renderJoinGroupButton()}
         {this.props.challenges.map(challenge => {
           return (
--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,8 @@ import Team from './routes/Team/TeamPage'
 import GroupPage from './routes/GroupPage'
 import NotFoundPage from './routes/NotFoundPage/NotFoundPage'
 import Leaderboard from './routes/Leaderboard/LeaderboardPage'
+import PrivateRoute from './Components/Utils/PrivateRoute'
+import PublicRoute from './Components/Utils/PublicRoute'
 
 export default class Routes extends React.Component {
   render() {
@@ -20,13 +22,13 @@ export default class Routes extends React.Component {
           <Route exact path='/' component={LandingPage}/>
           <Route path='/home' component={Home}/>
           <Route exact path='/groups/:group_id' component={GroupPage}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/register' component={Register}/>
-          <Route path='/challenge/:challenge_id' component={Challenge}/>
-          <Route path='/create-challenge' component={CreateChallenge}/>
-          <Route path='/create-group' component={CreateGroup}/>
-          <Route path='/team' component={Team}/>
-          <Route path='/groups/:group_id/leaderboard'  component={Leaderboard}/>
+          <PublicRoute path='/login' component={Login}/>
+          <PublicRoute path='/register' component={Register}/>
+          <PrivateRoute path='/challenge/:challenge_id' component={Challenge}/>
+          <PrivateRoute path='/create-challenge' component={CreateChallenge}/>
+          <PrivateRoute path='/create-group' component={CreateGroup}/>
+          <PrivateRoute path='/team' component={Team}/>
+          <PrivateRoute path='/groups/:group_id/leaderboard'  component={Leaderboard}/>
           <Route component={NotFoundPage}/>
         </Switch>
       </div>
