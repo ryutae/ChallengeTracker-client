eae810a56c38d7173d250d45c59162cd724127fa
ryuta <ryuta.eguchi@gmail.com>
2019-07-13 14:15:09 -0700

    Deploy to connect to heroku server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:2222/api',
+  API_ENDPOINT: `https://quiet-everglades-83020.herokuapp.com/api`,
+  API_ENDPOINT_LOCAL: 'http://localhost:8080/api',
   TOKEN_KEY: 'challenge-key',
+  API_KEY: process.env.REACT_APP_API_KEY
 }
