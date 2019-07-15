fe30e195c9c102750d6332798f47dc21ae9032eb
Ryuta Eguchi <ryuta.eguchi@gmail.com>
2019-07-15 11:43:37 -0700

    Point to deployed server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8080/api',
-  // `https://quiet-everglades-83020.herokuapp.com/api`,
+  API_ENDPOINT: `https://quiet-everglades-83020.herokuapp.com/api`,
   API_ENDPOINT_LOCAL: 'http://localhost:8080/api',
   TOKEN_KEY: 'challenge-key',
   API_KEY: process.env.REACT_APP_API_KEY
